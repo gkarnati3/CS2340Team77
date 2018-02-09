8c8a5851382e6ebeb6c55f0381123b6c8f573dc0
Alexander Hammond <ajhammond123@gmail.com>
2018-02-08 23:02:20 -0500

    Added User Class

---
+++ app/src/main/java/edu/gatech/cs2340/homefullshelter/Model/User.java
@@ -0,0 +1,24 @@
+package edu.gatech.cs2340.homefullshelter.Model;
+
+/**
+ * Created by AlexanderHammond on 2/8/18.
+ */
+
+public class User {
+    private final String username = "user";
+    private final String password = "pass";
+
+    public boolean checkUsername(String username) {
+        if (this.username.equals(username)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkPassword(String password) {
+        if (this.password.equals(password)) {
+            return true;
+        }
+        return false;
+    }
+}
