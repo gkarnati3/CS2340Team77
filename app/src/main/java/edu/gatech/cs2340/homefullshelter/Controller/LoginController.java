eb67809370c540bf7413a4bd330e06777cfeca4f
mattquan <quan.matthew1999@gmail.com>
2018-02-08 22:58:01 -0500

    added controller and model stub classes so the folders show up

---
+++ app/src/main/java/edu/gatech/cs2340/homefullshelter/Controller/LoginController.java
@@ -0,0 +1,8 @@
+package edu.gatech.cs2340.homefullshelter.Controller;
+
+/**
+ * Created by mattquan on 2/8/18.
+ */
+
+public class LoginController {
+}
---
+++ app/src/main/java/edu/gatech/cs2340/homefullshelter/Model/Model.java
@@ -0,0 +1,8 @@
+package edu.gatech.cs2340.homefullshelter.Model;
+
+/**
+ * Created by mattquan on 2/8/18.
+ */
+
+public class Model {
+}
