3d061a4d5dee401582183368c84250fd0fcb1331
mattquan <quan.matthew1999@gmail.com>
2018-02-27 12:11:26 -0500

    Added a Bottom Sheet onClick for each listview item. Added some functionality -- did the peek; the content i left a note for abhi to help him out. Deleted all unnecessary extra fies that Prat copied and pasted

--- app/src/main/java/edu/gatech/cs2340/homefullshelter/view/DataItemListActivity.java
+++ app/src/main/java/edu/gatech/cs2340/homefullshelter/view/DataItemListActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,7 +31,6 @@ import edu.gatech.cs2340.homefullshelter.model.Model;
  * An activity representing a list of Data Items. This activity
  * has different presentations for handset and tablet-size devices. On
  * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link DataItemDetailActivity} representing
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
  */
@@ -39,7 +40,6 @@ public class DataItemListActivity extends AppCompatActivity {
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
      */
-    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,27 +50,12 @@ public class DataItemListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
 
         View recyclerView = findViewById(R.id.dataitem_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
 
-        if (findViewById(R.id.dataitem_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-        }
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
@@ -94,30 +79,20 @@ public class DataItemListActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
             holder.mItem = mValues.get(position);
             holder.mIdView.setText("" + mValues.get(position).getKey());
             holder.mContentView.setText(mValues.get(position).getShelter());
-
+            LinearLayout bottomSheet = (LinearLayout) findViewById(R.id.linear_layout_bottom_sheet_theme_details);
+            final TextView peekText = (TextView) findViewById(R.id.textview_peek_shelterdetails);
+            final TextView contentText = (TextView) findViewById(R.id.textview_content_shelterdetails);
+            final BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
             holder.mView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (mTwoPane) {
-                        Bundle arguments = new Bundle();
-                        arguments.putInt(DataItemDetailFragment.ARG_ITEM_ID, holder.mItem.getKey());
-                        DataItemDetailFragment fragment = new DataItemDetailFragment();
-                        fragment.setArguments(arguments);
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.dataitem_detail_container, fragment)
-                                .commit();
-                    } else {
-                        Context context = v.getContext();
-                        Intent intent = new Intent(context, DataItemDetailActivity.class);
-                        Log.d("MYAPP", "Switch to detailed view for item: " + holder.mItem.getKey());
-                        intent.putExtra(DataItemDetailFragment.ARG_ITEM_ID, holder.mItem.getKey());
-
-                        context.startActivity(intent);
-                    }
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                    peekText.setText("" + mValues.get(position).getShelter());
+                    contentText.setText("ABHI DO THIS" + mValues.get(position).getKey());
                 }
             });
         }
