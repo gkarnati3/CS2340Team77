e9fff16a9d4c91d21094303f512b0551e2046e7c
Nitya Tarakad <nitarakad@gmail.com>
2018-03-13 02:37:01 -0400

    added in constraints for which beds can be added

--- app/src/main/java/edu/gatech/cs2340/homefullshelter/view/VacancyView.java
+++ app/src/main/java/edu/gatech/cs2340/homefullshelter/view/VacancyView.java
@@ -12,6 +12,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import edu.gatech.cs2340.homefullshelter.R;
 import edu.gatech.cs2340.homefullshelter.controller.DatabaseController;
 import edu.gatech.cs2340.homefullshelter.model.Shelter;
+import edu.gatech.cs2340.homefullshelter.model.User;
+import edu.gatech.cs2340.homefullshelter.model.Model;
 
 /**
  * Created by gkarnati3 on 3/11/18.
@@ -21,6 +23,7 @@ public class VacancyView extends AppCompatActivity {
     TextView name;
     TextView shelterView;
     TextView capacity;
+    TextView notIncrement;
     Button plus;
     public int countBeds = 0;
 
@@ -30,14 +33,20 @@ public class VacancyView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vacancy_view);
 
-        Shelter curr = getIntent().getExtras().getParcelable("Shelter");
+        final Shelter curr = getIntent().getExtras().getParcelable("Shelter");
         String capacityD = curr.getCapacity();
         final int capacityDecrease = Integer.parseInt(capacityD);
+        final Model model = Model.getInstance();
+        final User actualUser = model.getCurrentUser();
+        if (actualUser.getCurrentShelterID() == -1) {
+            actualUser.setCurrentShelterID(curr.getKey());
+        }
 
         name = findViewById(R.id.name);
         plus = findViewById(R.id.plus);
         shelterView = findViewById(R.id.shelterName);
         capacity = findViewById(R.id.capacity);
+        notIncrement = findViewById(R.id.notIncrement);
 
         if(curr != null) {
             shelterView.setText(curr.getName());
@@ -45,14 +54,18 @@ public class VacancyView extends AppCompatActivity {
         }
 
 
-        plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                countBeds++;
-                name.setText("" + countBeds);
-                capacity.setText("" + (capacityDecrease - countBeds));
-            }
-        });
+        if (actualUser.getCurrentShelterID() == curr.getKey()) {
+            plus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    countBeds++;
+                    name.setText("" + countBeds);
+                    capacity.setText("" + (capacityDecrease - countBeds));
+                }
+            });
+        } else {
+            notIncrement.setText("sorry! you can't add beds in the shelter you are not in");
+        }
 
 //        DatabaseController db = new DatabaseController();
 //        String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -62,7 +75,9 @@ public class VacancyView extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), ShelterListActivity.class));
-                //setNumberOfBeds(countBeds);
+                model.updateCurrentUser(actualUser);
+                actualUser.setNumberOfBeds(countBeds);
+                System.out.println(actualUser.getNumberOfBeds());
             }
         });
     }
