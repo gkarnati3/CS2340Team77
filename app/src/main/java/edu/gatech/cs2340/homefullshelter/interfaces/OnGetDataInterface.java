f9ffcdfef8c48e7d4604fd208726a7a0c011f9ea
Alexander Hammond <ajhammond123@gmail.com>
2018-03-29 22:22:31 -0400

    Javadoc Interface

--- app/src/main/java/edu/gatech/cs2340/homefullshelter/interfaces/OnGetDataInterface.java
+++ app/src/main/java/edu/gatech/cs2340/homefullshelter/interfaces/OnGetDataInterface.java
@@ -6,7 +6,16 @@ import com.google.firebase.database.DataSnapshot;
  * Created by AlexanderHammond on 3/12/18.
  */
 
+
 public interface OnGetDataInterface {
+    /**
+     * Method for successful data retrieval, parameter data will hold all retrieved data
+     * @param data all the data retrieved
+     */
     public void onDataRetrieved(DataSnapshot data);
+
+    /**
+     * Method to run on database failure
+     */
     public void onFailed();
 }
